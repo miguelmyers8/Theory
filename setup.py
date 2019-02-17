828327b561b26149993e77af97deaac7c8edac2a
miguel myers <myers.m007@gmail.com>
2019-02-17 13:49:14 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='theory',
+      version='0.1',
+      description='neural network library',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
